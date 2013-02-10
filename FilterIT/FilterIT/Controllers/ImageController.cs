6bb201c2453ac836a9008af9625379cab51dd9e6
Dimitar Pankov <mitelinko@gmail.com>
2013-02-10 02:01:47 +0200

    Implemented a filtering algorithm.
    Added an ImageController to the main project. That controller will
    process the HTTP POST request for filtering an image.
    The new Filtering project will apply the filters to the given image and
    will return the result as an ssv of "image strings".
    Added a very fast Invert Filter that uses BitmapData and has an unsafe
    block for direct memory access to the pixels of the image.

---
+++ FilterIT/FilterIT/Controllers/ImageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FilterIT;
+
+namespace FilterIT.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: /Image/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public object GetThumbnails(HttpPostedFileBase fileUpload)
+        {
+            var stream = fileUpload.InputStream;
+            Bitmap img = new Bitmap(stream);
+            Session["Image"] = img;
+            Session["ImageName"] = fileUpload.FileName;
+            int height = (int)(img.Height / ((float)img.Width / 500f));
+            img = ResizeBitmap(img, 500, height);
+            return img.ApplyFilters();
+        }
+
+        private Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+                g.DrawImage(sourceBMP, 0, 0, width, height);
+            return result;
+        }
+    }
+}
---
+++ FilterIT/Filtering/Filtering.cs
@@ -0,0 +1,47 @@
+﻿using Filtering;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Filtering;
+
+namespace FilterIT
+{
+    public static class Filtering
+    {
+        public static string ApplyFilters(this Bitmap bitmap)
+        {
+            string result = null;
+            BitmapData bitmapData = LockBitmap(bitmap);
+            result += InvertFilter.Apply(bitmapData).ToImageString();
+            bitmap.UnlockBits(bitmapData);
+            return result;
+        }
+
+        private static string ToImageString(this Bitmap img)
+        {
+            string bitmapString = null;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                img.Save(memoryStream, ImageFormat.Jpeg);
+                byte[] bitmapBytes = memoryStream.GetBuffer();
+                bitmapString = Convert.ToBase64String(bitmapBytes,
+                    Base64FormattingOptions.InsertLineBreaks);
+            }
+            bitmapString = "data:image/jpeg;base64," + bitmapString + " ";
+            return bitmapString;
+        }
+
+        private static BitmapData LockBitmap(Bitmap bitmap)
+        {
+            return bitmap.LockBits(new Rectangle(0, 0,
+                                    bitmap.Width, bitmap.Height),
+                                    ImageLockMode.ReadOnly,
+                                    PixelFormat.Format32bppArgb);
+        }
+    }
+}
