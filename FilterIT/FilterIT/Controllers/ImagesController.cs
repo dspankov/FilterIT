fb2bbcacc9a26f766516c38b6f9c3ece89e39c94
Dimitar Pankov <dspankov@gmail.com>
2013-03-10 20:21:03 +0200

    Added db.

---
+++ FilterIT/FilterIT/Controllers/ImagesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FilterIT.Models;
+
+namespace FilterIT.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ImageDBContext db = new ImageDBContext();
+
+        //
+        // GET: /Images/
+
+        public ActionResult Index()
+        {
+            return View(db.Images.ToList());
+        }
+
+        //
+        // GET: /Images/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        //
+        // GET: /Images/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Images/Create
+
+        [HttpPost]
+        public ActionResult Create(Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Images.Add(image);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(image);
+        }
+
+        //
+        // GET: /Images/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        //
+        // POST: /Images/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(image).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }
+
+        //
+        // GET: /Images/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        //
+        // POST: /Images/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Image image = db.Images.Find(id);
+            db.Images.Remove(image);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FilterIT/FilterIT/Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FilterIT.Models
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        public string Owner { get; set; }
+        public byte[] Data { get; set; }
+    }
+
+    public class ImageDBContext : DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+    }
+}
